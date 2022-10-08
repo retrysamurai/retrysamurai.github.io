298252aed9d26ddf3bb9c98e1b7109c773007984
retrysamurai <fateen.mostafa@gmail.com>
2022-10-08 22:03:07 +0600

    Added semicolon

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const getWinLoc = window.location.hostname;
 document.getElementById("url").innerHTML = getWinLoc;
 
-const getModDate = document.lastModified
-document.getElementById("modified").innerHTML = getModDate
+const getModDate = document.lastModified;
+document.getElementById("modified").innerHTML = getModDate;
